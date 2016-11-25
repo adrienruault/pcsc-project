b97746f93d0618eb81def9a47b8d5eaf22ca84e9
Guillaume Mollard <mollard@smapc012.epfl.ch>
2016-11-25 13:05:15 +0100

    first commit on Guillaume's account

---
+++ src/lena.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../CImg_latest/CImg.h"
+#include <cstdio>
+#include <jpeglib.h>
+#include <jerror.h>
+
+int main(int argc, char* argv[])
+{
+
+	std::cout << "Hello, World ! \n";
+
+	return 0;
+}
