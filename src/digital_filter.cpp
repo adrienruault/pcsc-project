d68754718210960533fd29677f499aa07c221b97
Guillaume Mollard <guillaume.mollard2@gmail.com>
2016-11-27 00:05:48 +0100

    some changes

--- src/digital_filter.cpp
+++ src/digital_filter.cpp
@@ -5,16 +5,19 @@ using namespace cimg_library;
 
 CImg<double> z_transform(const CImg<double>& img)
 {
+
+
+
   std::cout << "ok3" << std::endl;
-  CImg<double> imgtrans(img.width(),img.height(),1,1);
+  CImg<double> imgtrans(11,11,1,1);
   std::cout << "ok4" << std::endl;
-  for (size_t z1 = 0; z1 < img.width(); z1++) {
-    std::cout << z1;
-    for (size_t z2 = 0; z2 < img.height(); z2++) {
+  for (int z1 = 0; z1 < 11; z1++) {
+    std::cerr << z1;
+    for (int z2 = 0; z2 < 11; z2++) {
       imgtrans.operator()(z1,z2,0,0) = 0;
       for (size_t x = 0; x < img.width(); x++) {
         for (size_t y = 0; y < img.height(); y++) {
-          imgtrans.operator()(z1,z2,0,0) = img.operator()(x,y,0,0)*pow(z1,-x)*pow(z2,-y)*255;
+          imgtrans(z1,z2,0,0) += img(x,y,0,0)*pow(z1-5,-x+img.width()/2)*pow(z2-5,-y+img.height()/2);
         }
       }
 
--- src/lena.cpp
+++ src/lena.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
-#include "../CImg_latest/CImg.h"
+#include "CImg.h"
 #include <cstdio>
 #include <jpeglib.h>
 #include <jerror.h>
 
-int main(int argc, char* argv[])
+using namespace cimg_library;
+
+int main()
 {
 
-	std::cout << "Hello, World ! \n";
+	CImg<double> image("../ressources/ruault.jpg");
+	CImg<double> copy(image);
 
+	/*std::cout << copy.width() << copy.height() << copy.depth() <<  copy.spectrum() << copy.atXYZC(5,100,0,0,0) << '\n';
+	std::cout << copy.operator()(5,500);*/
+
+	for (size_t x = 0; x < copy.width(); x++) {
+		for (size_t y = 0; y < copy.height(); y++) {
+			for (size_t c = 0; c < 3; c++) {
+				if (copy.operator()(x,y,0,c) < 70)
+				{
+					copy.operator()(x,y,0,c) = 255-((255-70)/(70))*copy.operator()(x,y,0,c);
+				}
+			}
+
+
+		}
+	}
+	copy.save("ruault_solarized.jpg");
 	return 0;
 }
